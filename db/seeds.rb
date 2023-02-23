692c5708b228b2f39cef2be8bc452b1c78884c60
xup6u04cj86 <yenhualin@tamu.edu>
2023-02-22 22:39:32 -0600

    part4

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,7 @@ more_movies = [
     {:title => 'Nomadland', :rating => 'R',
       :release_date => '19-Feb-2021'},
     {:title => 'CODA', :rating => 'PG-13',
-      :release_date => '13-Aug-2021'}
+      :release_date => '13-Aug-2021'},
     {:title => 'Monsters, Inc.', :rating => 'G',
       :release_date => '02-Nov-2001'}
   ]
